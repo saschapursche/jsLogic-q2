77bb5bb3b2290c34f85aabb9061bee1ae47d44b6
saschapursche <sascha_pursche@outlook.de>
2023-05-15 21:58:26 +0200

    all possible arrays are output

--- jsLogic-q2.js
+++ jsLogic-q2.js
@@ -14,16 +14,18 @@
 
 //another try
 const array = [1, 2, 3, 4, 5, 6];
-const solution = [];
 const answer = (arr, target) => {
+const solution = [];
     for(let i = 0; i <= arr.length; i++) {
         for(let j = 0; j <= arr.length; j++){
             if((arr[i] + arr[j]) === target){
-                solution.push(arr[i], arr[j])
-                console.log(solution);
+                let btwArr = [];
+                    btwArr.push(arr[i], arr[j]);
+                    solution.push(btwArr);
             }
         }
     }
+    return solution;
 };
 answer(array, 7);
 
