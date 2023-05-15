f8da643b089c1c8ce2c054cfe52fbf67a147adf9
saschapursche <sascha_pursche@outlook.de>
2023-05-15 22:02:24 +0200

    cleaned .js files

--- jsLogic-q2.js
+++ jsLogic-q2.js
@@ -1,18 +1,3 @@
-// const answer = ([a], b) => {
-//     let n = a.length
-//     for (let i = 0; i < n; i++) {
-//         for(let j = 1+1; j < n; j++) {
-//             if(a[i] + a[j] === b) {
-//                 return(a[i], a[j]);
-//             }
-//         }
-//     }
-// };
-
-// answer([1, 2, 3], 4);
-// returns actually undefined
-
-//another try
 const array = [1, 2, 3, 4, 5, 6];
 const answer = (arr, target) => {
 const solution = [];
@@ -28,8 +13,3 @@ const solution = [];
     return solution;
 };
 answer(array, 7);
-
-
-//Question 2: Write a javascript function that takes an array of numbers and a target number.
-//The function should find two different numbers in the array that, when added together, give the target number.
-//For example: answer([1,2,3], 4)should return [1,3]
